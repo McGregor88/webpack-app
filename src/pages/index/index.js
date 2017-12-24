710e2dd3bfc9e01635329165302466c0920dec0e
Ruslan <ruslan.igorevich@list.ru>
2017-12-24 17:35:03 +0300

    add sass plugins

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,3 +1,5 @@
+import './index.scss';
+
 import createMenu from '../../components/menu/menu';
 var menu = createMenu(['Главная', 'Новости'], 'header-menu');
 document.body.appendChild(menu);
--- src/pages/news/news.js
+++ src/pages/news/news.js
@@ -1,3 +1,5 @@
+import './news.scss';
+
 import createMenu from '../../components/menu/menu';
 var menu = createMenu(['Главная', 'Новости'], 'header-menu');
 document.body.appendChild(menu);
